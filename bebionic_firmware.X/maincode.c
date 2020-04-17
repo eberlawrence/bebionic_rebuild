05238a454d89b81307de302a3311d9644d61ec7c
Eber Lawrence <eberlawrence@hotmail.com>
2020-04-17 20:20:09 -0300

    Updating

--- bebionic_firmware.X/maincode.c
+++ bebionic_firmware.X/maincode.c
@@ -149,7 +149,7 @@ void PWM(unsigned int speed, char direction[10]){
     PORTBbits.RB14 = 0;
     P1TCON = 0x0002;
     P1TMR = 0;
-    P1TPER = 0xFFFF;
+    P1TPER = 0x7FFE; // 56.23 Hz // (range => 1 - 7FFE)
     P1DTCON1 = 0x003B;
 
     
@@ -157,15 +157,16 @@ void PWM(unsigned int speed, char direction[10]){
     y = (P1TPER / 100) * x;
 
     
-    
     if (strcmp(direction, "forward") == 0){
         PWM1CON1 = 0x0010;
         P1DC1 = y;
+        PORTBbits.RB12 = 0;
     }
     else if (strcmp(direction, "backward") == 0)
     {
         PWM1CON1 = 0x0020;
         P1DC2 = y;
+        PORTBbits.RB14 = 0;
     }
     else if (strcmp(direction, "stop") == 0){
         PWM1CON1 = 0x0030;
@@ -251,14 +252,13 @@ int main(void) {
     PORTBbits.RB13 =     1;
     
     
-    
     while(1){
         if (task == 1){
             PWM(50, "forward");
             task = 0;
         }
         if (task == 2){
-            PWM(100, "stop");
+            PWM(50, "stop");
             task = 0;
         }
         
