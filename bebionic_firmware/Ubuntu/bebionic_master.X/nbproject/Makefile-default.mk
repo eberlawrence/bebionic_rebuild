ff0c259417d01bdeb939ed76589ee498eef16f58
eberlawrence <eberlawrence@ufu.br>
2021-12-04 00:30:06 +0000

    Updating

--- bebionic_firmware/Ubuntu/bebionic_master.X/nbproject/Makefile-default.mk
+++ bebionic_firmware/Ubuntu/bebionic_master.X/nbproject/Makefile-default.mk
@@ -28,13 +28,13 @@ CP=cp
 CND_CONF=default
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 IMAGE_TYPE=debug
-OUTPUT_SUFFIX=elf
-DEBUGGABLE_SUFFIX=elf
+OUTPUT_SUFFIX=cof
+DEBUGGABLE_SUFFIX=cof
 FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 else
 IMAGE_TYPE=production
 OUTPUT_SUFFIX=hex
-DEBUGGABLE_SUFFIX=elf
+DEBUGGABLE_SUFFIX=cof
 FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 endif
 
@@ -95,48 +95,42 @@ MP_LINKER_FILE_OPTION=,--script=p33FJ32MC204.gld
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/725218671/main_master.o: ../../current_files/main_master.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/725218671/main_master.o: ../../current_files/main_master.c  .generated_files/flags/default/ea039ea76bce7d79a331e5f0ec137eb85c6e1be4 .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/725218671" 
 	@${RM} ${OBJECTDIR}/_ext/725218671/main_master.o.d 
 	@${RM} ${OBJECTDIR}/_ext/725218671/main_master.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/main_master.c  -o ${OBJECTDIR}/_ext/725218671/main_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/725218671/main_master.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/725218671/main_master.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/main_master.c  -o ${OBJECTDIR}/_ext/725218671/main_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/725218671/main_master.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/_ext/1673019547/I2C_master.o: ../../current_files/libraries/I2C_master.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1673019547/I2C_master.o: ../../current_files/libraries/I2C_master.c  .generated_files/flags/default/2d1eed3228aa6819518313c955c7c6aa44929c5e .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/1673019547" 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/I2C_master.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/I2C_master.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/I2C_master.c  -o ${OBJECTDIR}/_ext/1673019547/I2C_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/I2C_master.c  -o ${OBJECTDIR}/_ext/1673019547/I2C_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/_ext/1673019547/motor_pwm.o: ../../current_files/libraries/motor_pwm.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1673019547/motor_pwm.o: ../../current_files/libraries/motor_pwm.c  .generated_files/flags/default/346d821d141b69eebb847468f7238fbfb78c838c .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/1673019547" 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/motor_pwm.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/motor_pwm.c  -o ${OBJECTDIR}/_ext/1673019547/motor_pwm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/motor_pwm.c  -o ${OBJECTDIR}/_ext/1673019547/motor_pwm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1    -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 else
-${OBJECTDIR}/_ext/725218671/main_master.o: ../../current_files/main_master.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/725218671/main_master.o: ../../current_files/main_master.c  .generated_files/flags/default/9dbf47f7d02c6fe1f32d2a3923cb0cba0d799cb7 .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/725218671" 
 	@${RM} ${OBJECTDIR}/_ext/725218671/main_master.o.d 
 	@${RM} ${OBJECTDIR}/_ext/725218671/main_master.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/main_master.c  -o ${OBJECTDIR}/_ext/725218671/main_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/725218671/main_master.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/725218671/main_master.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/main_master.c  -o ${OBJECTDIR}/_ext/725218671/main_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/725218671/main_master.o.d"        -g -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/_ext/1673019547/I2C_master.o: ../../current_files/libraries/I2C_master.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1673019547/I2C_master.o: ../../current_files/libraries/I2C_master.c  .generated_files/flags/default/59f6d75a6f31068a1d1622ab621f1da380ce6a93 .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/1673019547" 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/I2C_master.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/I2C_master.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/I2C_master.c  -o ${OBJECTDIR}/_ext/1673019547/I2C_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/I2C_master.c  -o ${OBJECTDIR}/_ext/1673019547/I2C_master.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1673019547/I2C_master.o.d"        -g -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/_ext/1673019547/motor_pwm.o: ../../current_files/libraries/motor_pwm.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/_ext/1673019547/motor_pwm.o: ../../current_files/libraries/motor_pwm.c  .generated_files/flags/default/a0fc0f14cf73470cd531e397cacf03b688ed8b8e .generated_files/flags/default/3b94104c82cf0fd39bbff45abc1dbc073067cc36
 	@${MKDIR} "${OBJECTDIR}/_ext/1673019547" 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1673019547/motor_pwm.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/motor_pwm.c  -o ${OBJECTDIR}/_ext/1673019547/motor_pwm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp=${DFP_DIR}/xc16
-	@${FIXDEPS} "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../../current_files/libraries/motor_pwm.c  -o ${OBJECTDIR}/_ext/1673019547/motor_pwm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/1673019547/motor_pwm.o.d"        -g -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 endif
 
@@ -157,13 +151,13 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)      -Wl,,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp=${DFP_DIR}/xc16 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)      -Wl,,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -Wl,,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp=${DFP_DIR}/xc16 
-	${MP_CC_DIR}/xc16-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf   -mdfp=${DFP_DIR}/xc16 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=coff -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -Wl,,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST)  -mdfp="${DFP_DIR}/xc16" 
+	${MP_CC_DIR}/xc16-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/bebionic_master.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=coff   -mdfp="${DFP_DIR}/xc16" 
 	
 endif
 
--- bebionic_firmware/current_files/main_master.c
+++ bebionic_firmware/current_files/main_master.c
@@ -52,238 +52,238 @@ _Bool stop_mov = 0;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-void power_grasp(){
-    if (CH_A) {
-        send_command(index_addr, 80);
-        send_command(middle_addr, 80);
-        send_command(ring_addr, 80);
-        send_command(pinky_addr, 80);
-        motor_pwm_config(50, "forward");
-    }
-    else if (CH_B) {
-        send_command(index_addr, 90);
-        send_command(middle_addr, 90);
-        send_command(ring_addr, 90);
-        send_command(pinky_addr, 90);
-        motor_pwm_config(50, "backward");
-    }
-
-}
-void tripod_grasp(){
-    
-}
-void finger_point_grasp(){
-    
-}
-void key_grasp(){
-    
-}
-
-void grasp_selection(){
-    if (MAG_SEN){
-        if (grasp == 0){
-            
-            power_grasp();
-        }
-        else {
-            tripod_grasp();
-        }        
-    }
-    else {
-        if (grasp == 0){
-            _RC9 = 1;
-            power_grasp();
-        }
-        else {
-            key_grasp();
-        }          
-    }
-}
-
-void turn_on_feedback(void){        
-        VIB_CALL = 1;
-        BUZZER = 1;
-        __delay_ms(300);
-        VIB_CALL = 0;
-        BUZZER = 0;
-}
-void turn_off_feedback(void){        
-        VIB_CALL = 1;
-        __delay_ms(200);
-        VIB_CALL = 0;
-        __delay_ms(200);
-        VIB_CALL = 1;
-        __delay_ms(300);
-        VIB_CALL = 0;
-}
-void change_grasp_block_feedback(void){        
-        BUZZER = 1;
-        __delay_ms(300);
-        BUZZER = 0;
-}
-void do_something_feedback(void){        
-        BUZZER = 1;
-        __delay_ms(200);
-        BUZZER = 0;
-        __delay_ms(100);
-        BUZZER = 1;
-        __delay_ms(200);
-        BUZZER = 0;
-}
-void do_anotherthing_feedback(void){        
-        BUZZER = 1;
-        __delay_ms(100);
-        BUZZER = 0;
-        __delay_ms(50);
-        BUZZER = 1;
-        __delay_ms(100);
-        BUZZER = 0;
-        __delay_ms(50);
-        BUZZER = 1;
-        __delay_ms(100);
-        BUZZER = 0;
-}
-
-
-/* Motor's encoder interruption - Initializer */
-void interrupt0_Init(void)
-{
-    _INT0EP = 1; // negative/positive edge detect polarity              - NEGATIVE
-    _INT0IE = 1; // enable/disable external interrupt                   - ENABLE
-    _INT0IP = 1; // 3-bit (0 to 7) interrupt priority config            - 001  
-}
-/* Movement interruption by HIGH level on channel A or B - Initializer */
-void interrupt1_Init(void)
-{
-    _INT1R  = 17; // set the RPx as external interrupt pin               - RP17
-    _INT1EP = 1;  // negative/positive edge detect polarity              - NEGATIVE
-    _INT1IE = 1;  // enable/disable external interrupt                   - ENABLE
-    _INT1IP = 1;  // 3-bit (0 to 7) interrupt priority config            - 010
-}
-/* Main button interruption - Initializer */
-void interrupt2_Init(void)
-{
-    _INT2R  = 24; // set the RPx as external interrupt pin              - RP24
-    _INT2EP = 1;  // negative/positive edge detect polarity             - NEGATIVE
-    _INT2IE = 1;  // enable/disable external interrupt                  - ENABLE
-    _INT2IP = 1;  // 3-bit (0 to 7) interrupt priority config           - 010
-}
-/* Timer1 interruption - Initializer */
-void timer1_Init(void)
-{
-    PR1             = 36850; // set the period of timer1                         - 10 ms
-    T1CONbits.TCKPS = 0;     // define prescaler (0=1:1, 1=1:8, 2=1:64, 3=1:256) - 1:1
-    _T1IE           = 1;     // eneble/disable timer1 interrupt                  - ENABLE
-    T1CONbits.TON   = 0;     // start/stop timer1                                - STOP
-}
-
-/* Do it when extern interruption 0 happens */
-void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void)
-{
-    if (ENC_A){
-        // forward
-        limit = 0;
-        pulse++;
-        if (pulse == 500)
-        {
-            motor_pwm_config(0, "off");
-            limit = 1;
-        } 
-    }
-    else if (!ENC_A){
-        // backward
-        limit = 0;
-        pulse--;
-        if (pulse == 0)
-        {
-            motor_pwm_config(0, "off");
-            limit = 1;
-        }
-    } 
-    _INT0IF = 0;
-}
-/* Do it when extern interruption 1 happens */
-void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void)               
-{           
-    if (CH_A & CH_B){        
-        motor_pwm_config(0, "off");
-    }
-    else if (CH_A){
-        grasp_selection();
-    }
-    else if (CH_B){
-        if (repeated & limit){
-            grasp = abs(grasp - 1); // invert the grasp value
-            repeated = 0;
-        }
-        else{
-            grasp_selection();
-            repeated = 1;
-        } 
-    }
-    stop_mov = 1;
-    _INT1IF = 0;
-}
-/* Do it when extern interruption 2 happens */
-void __attribute__((interrupt, no_auto_psv)) _INT2Interrupt(void)               
-{   
-    T1CONbits.TON = 1;
-    _INT2IF = 0;
-}
-/* Do it when timer1 interruption happens */
-void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
-{    
-    if (!on){ // if the button is pressed and the prosthesis was off        
-        on = 1; // Turn the prosthesis on
-        turn_on_feedback();
-        T1CONbits.TON = 0; // disable the timer  
-        _RA4 = 1; // enable finger's motor supply
-        _RA7 = 1; // disable SLEEP MODE MOTOR DRIVER
-        _RA9 = 1; // disable POWER SAVE MODE
-        _RC3 = 1; // enable finger's supply
-        _RC4 = 1; // enable finger's supply
-    }
-        
-    else if (on){ // if the button is pressed and the prosthesis was on        
-        timer1 += 10;
-        if (timer1 > 4000){
-            on = 0; // Turn the prosthesis off
-            turn_off_feedback();      
-            timer1 = 0; // timer reset 
-            T1CONbits.TON = 0; // disable the timer
-            _RA4 = 0; // disable finger's motor supply
-            _RA7 = 0; // enable SLEEP MODE MOTOR DRIVER
-            _RA9 = 0; // enable POWER SAVE MODE
-            _RC3 = 0; // disable finger's supply
-            _RC4 = 0; // disable finger's supply
-        }
-    }
-    
-    if ((MAIN_BUT) & (timer1 != 0)) {
-        
-        if (timer1 <= 800){ // This will be used to change grasp block.
-            // insert function or flag to change the grasp block
-            change_grasp_block_feedback();        
-            timer1 = 0;
-            T1CONbits.TON = 0;
-        }
-        
-        // This could be used to change configs, e.g. changing the kind of control of the prosthesis.
-        else if ((timer1 > 800) & (timer1 <= 2000)){
-            do_something_feedback(); 
-            timer1 = 0;
-            T1CONbits.TON = 0; 
-        }
-        else if ((timer1 > 2000) & (timer1 <= 4000)){
-            do_anotherthing_feedback(); 
-            timer1 = 0;
-            T1CONbits.TON = 0; 
-        }
-    }
-    _T1IF = 0;
-}
-
-
+//void power_grasp(){
+//    if (CH_A) {
+//        send_command(index_addr, 80);
+//        send_command(middle_addr, 80);
+//        send_command(ring_addr, 80);
+//        send_command(pinky_addr, 80);
+//        motor_pwm_config(50, "forward");
+//    }
+//    else if (CH_B) {
+//        send_command(index_addr, 90);
+//        send_command(middle_addr, 90);
+//        send_command(ring_addr, 90);
+//        send_command(pinky_addr, 90);
+//        motor_pwm_config(50, "backward");
+//    }
+//
+//}
+//void tripod_grasp(){
+//    
+//}
+//void finger_point_grasp(){
+//    
+//}
+//void key_grasp(){
+//    
+//}
+//
+//void grasp_selection(){
+//    if (MAG_SEN){
+//        if (grasp == 0){
+//            
+//            power_grasp();
+//        }
+//        else {
+//            tripod_grasp();
+//        }        
+//    }
+//    else {
+//        if (grasp == 0){
+//            _RC9 = 1;
+//            power_grasp();
+//        }
+//        else {
+//            key_grasp();
+//        }          
+//    }
+//}
+//
+//void turn_on_feedback(void){        
+//        VIB_CALL = 1;
+//        BUZZER = 1;
+//        __delay_ms(300);
+//        VIB_CALL = 0;
+//        BUZZER = 0;
+//}
+//void turn_off_feedback(void){        
+//        VIB_CALL = 1;
+//        __delay_ms(200);
+//        VIB_CALL = 0;
+//        __delay_ms(200);
+//        VIB_CALL = 1;
+//        __delay_ms(300);
+//        VIB_CALL = 0;
+//}
+//void change_grasp_block_feedback(void){        
+//        BUZZER = 1;
+//        __delay_ms(300);
+//        BUZZER = 0;
+//}
+//void do_something_feedback(void){        
+//        BUZZER = 1;
+//        __delay_ms(200);
+//        BUZZER = 0;
+//        __delay_ms(100);
+//        BUZZER = 1;
+//        __delay_ms(200);
+//        BUZZER = 0;
+//}
+//void do_anotherthing_feedback(void){        
+//        BUZZER = 1;
+//        __delay_ms(100);
+//        BUZZER = 0;
+//        __delay_ms(50);
+//        BUZZER = 1;
+//        __delay_ms(100);
+//        BUZZER = 0;
+//        __delay_ms(50);
+//        BUZZER = 1;
+//        __delay_ms(100);
+//        BUZZER = 0;
+//}
+//
+//
+///* Motor's encoder interruption - Initializer */
+//void interrupt0_Init(void)
+//{
+//    _INT0EP = 1; // negative/positive edge detect polarity              - NEGATIVE
+//    _INT0IE = 1; // enable/disable external interrupt                   - ENABLE
+//    _INT0IP = 1; // 3-bit (0 to 7) interrupt priority config            - 001  
+//}
+///* Movement interruption by HIGH level on channel A or B - Initializer */
+//void interrupt1_Init(void)
+//{
+//    _INT1R  = 17; // set the RPx as external interrupt pin               - RP17
+//    _INT1EP = 1;  // negative/positive edge detect polarity              - NEGATIVE
+//    _INT1IE = 1;  // enable/disable external interrupt                   - ENABLE
+//    _INT1IP = 1;  // 3-bit (0 to 7) interrupt priority config            - 010
+//}
+///* Main button interruption - Initializer */
+//void interrupt2_Init(void)
+//{
+//    _INT2R  = 24; // set the RPx as external interrupt pin              - RP24
+//    _INT2EP = 1;  // negative/positive edge detect polarity             - NEGATIVE
+//    _INT2IE = 1;  // enable/disable external interrupt                  - ENABLE
+//    _INT2IP = 1;  // 3-bit (0 to 7) interrupt priority config           - 010
+//}
+///* Timer1 interruption - Initializer */
+//void timer1_Init(void)
+//{
+//    PR1             = 36850; // set the period of timer1                         - 10 ms
+//    T1CONbits.TCKPS = 0;     // define prescaler (0=1:1, 1=1:8, 2=1:64, 3=1:256) - 1:1
+//    _T1IE           = 1;     // eneble/disable timer1 interrupt                  - ENABLE
+//    T1CONbits.TON   = 0;     // start/stop timer1                                - STOP
+//}
+//
+///* Do it when extern interruption 0 happens */
+//void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void)
+//{
+//    if (ENC_A){
+//        // forward
+//        limit = 0;
+//        pulse++;
+//        if (pulse == 500)
+//        {
+//            motor_pwm_config(0, "off");
+//            limit = 1;
+//        } 
+//    }
+//    else if (!ENC_A){
+//        // backward
+//        limit = 0;
+//        pulse--;
+//        if (pulse == 0)
+//        {
+//            motor_pwm_config(0, "off");
+//            limit = 1;
+//        }
+//    } 
+//    _INT0IF = 0;
+//}
+///* Do it when extern interruption 1 happens */
+//void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void)               
+//{           
+//    if (CH_A & CH_B){        
+//        motor_pwm_config(0, "off");
+//    }
+//    else if (CH_A){
+//        grasp_selection();
+//    }
+//    else if (CH_B){
+//        if (repeated & limit){
+//            grasp = abs(grasp - 1); // invert the grasp value
+//            repeated = 0;
+//        }
+//        else{
+//            grasp_selection();
+//            repeated = 1;
+//        } 
+//    }
+//    stop_mov = 1;
+//    _INT1IF = 0;
+//}
+///* Do it when extern interruption 2 happens */
+//void __attribute__((interrupt, no_auto_psv)) _INT2Interrupt(void)               
+//{   
+//    T1CONbits.TON = 1;
+//    _INT2IF = 0;
+//}
+///* Do it when timer1 interruption happens */
+//void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
+//{    
+//    if (!on){ // if the button is pressed and the prosthesis was off        
+//        on = 1; // Turn the prosthesis on
+//        turn_on_feedback();
+//        T1CONbits.TON = 0; // disable the timer  
+//        _RA4 = 1; // enable finger's motor supply
+//        _RA7 = 1; // disable SLEEP MODE MOTOR DRIVER
+//        _RA9 = 1; // disable POWER SAVE MODE
+//        _RC3 = 1; // enable finger's supply
+//        _RC4 = 1; // enable finger's supply
+//    }
+//        
+//    else if (on){ // if the button is pressed and the prosthesis was on        
+//        timer1 += 10;
+//        if (timer1 > 4000){
+//            on = 0; // Turn the prosthesis off
+//            turn_off_feedback();      
+//            timer1 = 0; // timer reset 
+//            T1CONbits.TON = 0; // disable the timer
+//            _RA4 = 0; // disable finger's motor supply
+//            _RA7 = 0; // enable SLEEP MODE MOTOR DRIVER
+//            _RA9 = 0; // enable POWER SAVE MODE
+//            _RC3 = 0; // disable finger's supply
+//            _RC4 = 0; // disable finger's supply
+//        }
+//    }
+//    
+//    if ((MAIN_BUT) & (timer1 != 0)) {
+//        
+//        if (timer1 <= 800){ // This will be used to change grasp block.
+//            // insert function or flag to change the grasp block
+//            change_grasp_block_feedback();        
+//            timer1 = 0;
+//            T1CONbits.TON = 0;
+//        }
+//        
+//        // This could be used to change configs, e.g. changing the kind of control of the prosthesis.
+//        else if ((timer1 > 800) & (timer1 <= 2000)){
+//            do_something_feedback(); 
+//            timer1 = 0;
+//            T1CONbits.TON = 0; 
+//        }
+//        else if ((timer1 > 2000) & (timer1 <= 4000)){
+//            do_anotherthing_feedback(); 
+//            timer1 = 0;
+//            T1CONbits.TON = 0; 
+//        }
+//    }
+//    _T1IF = 0;
+//}
+//
+//
 
 
 /*  */
@@ -314,16 +314,23 @@ void main_init(void)
     _TRISC7  = 0; // OUTPUT - enable/disable PWM2L1 to drive buzzer to deliver auditory feedback to the user
     _TRISC8  = 1; // INPUT  - set main button (on/off - change prosthesis mode - etc)
     
-    timer1_Init();
-    interrupt0_Init();
-    interrupt1_Init();
-    interrupt2_Init();  
+//    timer1_Init();
+//    interrupt0_Init();
+//    interrupt1_Init();
+//    interrupt2_Init();  
     i2c_Init(100000);
 }
 
 /*  */
 int main(void) {
     main_init();
+    _RA4 = 1;
+    _RA7 = 1;
+    send_command(index_addr, 90);
+    send_command(middle_addr, 90);
+    send_command(ring_addr, 90);
+    send_command(pinky_addr, 90);
+    motor_pwm_config(50, "backward");
     while(1){
 //        if (!CH_A & !CH_B & stop_mov){
 //            _RB9 = 1;
